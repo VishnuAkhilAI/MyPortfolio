f9f0a87e242e74dfb52f1a76d1b6b22129636bc7
Vishnu <vishnu.akhil10@gmail.com>
2021-02-09 10:44:03 +1100

    Initial Commit

---
+++ MyPortfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyPortfolio
+//
+//  Created by Vishnu Akhil Upparapalle on 9/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyPortfolio/MyPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPortfolioApp.swift
+//  MyPortfolio
+//
+//  Created by Vishnu Akhil Upparapalle on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MyPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
